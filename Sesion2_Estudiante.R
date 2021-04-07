4b358c722b40ab3d8fcf3a655347375b94f00d81
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2021-04-07 08:54:20 -0500

    data structure

--- Sesion2_Estudiante.R
+++ Sesion2_Estudiante.R
@@ -1,3 +1,6 @@
+rm(list = ls()) # limpiar valiables
+
+
 # Verificar directorio de trabajo
 #===============================================================================
 getwd()
@@ -39,21 +42,29 @@ setwd("C:/Users/CLUQUE2.D/Documents/Analisis de bases de datos en R/Sesion2")
 
 #Crear un vector numérico.
 
+v_num = c(1,2,7,4,5,11)
+class(v_en)
 
 #Crear un vector entero.
 
+v_en = c(1L,3L,5L)
+class(v_en)
 
 #Crear un vector lógico
-
+v_log = c(T,F,T,F,T)
+class(v_log)
 
 #Crear un vector tipo carácter.
-
+v_car = c("tito","valentina")
+class(v_car)
 
 
 #Utilizar la función typeof(vector) para saber de que tipo es el vector 
 #Comprobar si es de un tipo particular mediante la familia de funciones 
 #is.character, is.double, is.integer, is.logical, is.atomic.
 
+is.character(v_num)
+is.integer(v_en)
 
 
 
@@ -69,22 +80,63 @@ setwd("C:/Users/CLUQUE2.D/Documents/Analisis de bases de datos en R/Sesion2")
 #Ejercicio
 #Comprobar lo anterior definiendo un vector y revisando su tipo.
 
+v_prueba = c(1,2,"abc") #vector character
+class(v_prueba)
+
+v_prueba1 = c(T,F,3L)  #vector integer
+class(v_prueba1)
+
+v_prueba2 = c(1,2,3,4,5)
+class(v_prueba2)
+
+v_prueba2 = as.character(v_prueba2)
+class(v_prueba2)
+
 
 #Ejercicio
+
+x1 = c(12,13,14,15,16,11,12,13,14)
+n = length(c1);n
+
 #Extraer elementos de un vector.
 
+x1[3]
+
+x1[c(3,5)]
+
+x1[-1]
+
+x1[-c(1,n)]
 
 
 #Especificar condiciones lógicas sobre un vector 
 
+x1>13
+x1[x1>13]
 
+x1 != 12
+x1[x1 != 12]
 
 #Crear una secuencias
 
+x2 = seq(1:10)
+
+x3 = seq(1,25, by=3)
+x3
+
+x4 = rep(1,5)
+x4
+
+x5 = rep(c(1,2),6)
+x5
+
+x6 = c(2,3,4,NA,NA,11)
+is.na(x6)
+x6[!is.na(x6)]
 
 
 #Calcular la longitud de un vector
-
+length(x5)
 
 
 
@@ -97,8 +149,11 @@ setwd("C:/Users/CLUQUE2.D/Documents/Analisis de bases de datos en R/Sesion2")
 #Los factores están caracterizados por dos atributos: class y levels
 
 
-
-
+x7 = factor(c("O","O","B","AB","B","A","AB"))
+class(x7)
+levels (x7)
+summary (x7)
+levels(x1)
 
 #===============================================================================
 #===============================================================================
@@ -107,20 +162,24 @@ setwd("C:/Users/CLUQUE2.D/Documents/Analisis de bases de datos en R/Sesion2")
 #Una matriz en R es un conjunto de objetos indizados por filas y columnas. 
 #Un array en R es lo mismo, salvo que puede tener más de dos dimensiones.
 
-#La sintaxis general de la orden para crear una matriz es la siguiente:
+#La sintaxis general del orden para crear una matriz es la siguiente:
 #matrix(data, nrow, ncol, byrow=F)
 
 #Ejercicio: Construir algunas matrices 
-
-
+A = matrix(1:6);A
+B = matrix(1:6,nrow=2);B
+C = matrix(1:6,nrow=2,byrow=T);C
 
 #Ejercicio: Extaer elementos de una matriz 
-
-
+C[2,3]
+C[,1]
+C[1,]
 
 #Ejercicio: Asignar nombres a las filas y columnas de una matriz.
 
-
+rownames(C) = c("s1","s2")
+colnames(C) = c("v1","v2","v3")
+C
 
 #Funciones para conocer atributos de las matrices 
 #dim()	devuelve las dimensiones de una matriz
@@ -132,6 +191,12 @@ setwd("C:/Users/CLUQUE2.D/Documents/Analisis de bases de datos en R/Sesion2")
 
 
 #Ejercicio: Determine los atributos de la matriz B.
+dim(C)
+colnames(C)
+rownames(C)
+mode(C)
+length(C)
+is.matrix(C)
 
 
 
